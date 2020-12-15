34c4162a4c869109c9639d1cd15a0d421aa99431
esthergutierrezr <your-email-address>
2020-12-15 17:37:19 +0000

    locations page component created

---
+++ src/locationsPage/LocationsCard/LocationsCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LocationsCard() {
+  return <div></div>;
+}
+
+export default LocationsCard;
---
+++ src/locationsPage/LocationsPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LocationsPage() {
+  return <div></div>;
+}
+
+export default LocationsPage;
