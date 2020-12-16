bd40966e5956deb7a4b827204236537e819798a1
esthergutierrezr <your-email-address>
2020-12-16 10:21:22 +0000

    folder deleted

--- src/locationsPage/LocationsCard/LocationsCard.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function LocationsCard() {
-  return <div></div>;
-}
-
-export default LocationsCard;
--- src/locationsPage/LocationsPage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function LocationsPage() {
-  return <div></div>;
-}
-
-export default LocationsPage;
