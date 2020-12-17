1c47367588a434825821b81bb69955a7cf678ac0
esthergutierrezr <your-email-address>
2020-12-17 12:05:58 +0000

    starting

---
+++ src/components/Locations/LocationsCard/LocationsCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function LocationsCard() {
+  return (
+    <div>
+      <h1>Location Name</h1>
+      <div>
+        <p>Wifi Details</p>
+        <p>Network:</p>
+        <p>Password:</p>
+      </div>
+      <div>Closing at </div>
+      <button>Check Out</button>
+    </div>
+  );
+}
+
+export default LocationsCard;
