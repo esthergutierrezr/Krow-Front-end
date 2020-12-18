c32ab33df94c4f39ac6bfe4f042e639aef797f3e
LuisClara92 <lc.sbg.92@gmail.com>
2020-12-18 09:40:50 +0000

    save to clone new repo

--- src/components/Profile/EditProfile.jsx
+++ src/components/Profile/EditProfile.jsx
@@ -12,61 +12,120 @@
 // Company
 // Button Save details
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
+import { NavLink as Link } from "react-router-dom";
 import Arrow from "./arrow+left.png";
 import { Header } from "./Styles";
 
 class EditProfile extends Component {
   state = {
-    title: "",
-    director: "",
-    year: "",
-    duration: "",
+    username: this.props.username,
+    email: this.props.email,
+    phone: this.props.phone,
+    city: this.props.city,
+    profession: this.props.profession,
+    company: this.props.company,
+    industry:this.props.industry,
+    inEditMode: true,
+    id: this.props.id,
   };
 
   handleChange = (event) => {
     const { name, value } = event.target;
-    //console.log(name, value);
     this.setState({ [name]: value });
   };
+
   render() {
-    const { title, director, year, duration } = this.state;
+    const {
+      id,
+      username,
+      email,
+      phone,
+      city,
+      profession,
+      company,
+      insdustry,
+      editUser,
+    } = this.props;
+
+    const { inEditMode } = this.state;
+
     return (
       <div>
-         <Header>
-           <Link></Link>
-    <img src={Arrow} className="arrowleft" />
-    <h1>Edit Profile</h1>
-  </Header>
-
-        <form
-          onSubmit={(event) => this.props.addMovie(event, this.state)}
-          style={{ padding: "10px", border: "1px black solid" }}
-        >
-          <label>Title</label>
-          <input onChange={this.handleChange} value={title} name="title" />
-          <br />
-          <label>Director</label>
-          <input
-            onChange={this.handleChange}
-            value={director}
-            name="director"
-          />
-          <br />
-          <label>Year</label>
-          <input onChange={this.handleChange} value={year} name="year" />
-          <br />
-          <label>Duration</label>
-          <input onChange={this.handleChange} value={duration} name="duration" />
-          <br />
-          <button type="submit">Add Movie</button>
-        </form>
+        <Header>
+          <Link to="/profile">
+            <img src={Arrow} className="arrowleft" />
+          </Link>
+          <h1>Edit Profile</h1>
+        </Header>
+        <div style={{ margin: "15px", border: "1px black solid" }}>
+          {inEditMode ? (
+            <div>
+              <form
+                onSubmit={(event) => {
+                  editUser(event, this.state);
+                  this.setState({ inEditMode: false });
+                }}
+              >
+                <label>email</label>
+                <input
+                  onChange={this.handleChange}
+                  defaultValue={email}
+                  replace="email"
+                  name="email"
+                />
+                <br />
+                <label>username</label>
+                <input
+                  onChange={this.handleChange}
+                  defaultValue={username}
+                  name="username"
+                />
+                <br />
+                <label>email</label>
+                <input
+                  onChange={this.handleChange}
+                  defaultValue={email}
+                  name="email"
+                />
+                <br />
+                <label>phone</label>
+                <input
+                  onChange={this.handleChange}
+                  defaultValue={phone}
+                  name="phone"
+                />
+                <br />
+                <label>website</label>
+                <input
+                  onChange={this.handleChange}
+                  defaultValue={website}
+                  name="website"
+                />
+                <br />
+                <button type="submit">Save Details</button>
+              </form>
+            </div>
+          ) : (
+            <div style={{ margin: "10px" }}>
+              <p>Name: {name}</p>
+              <p>Username: {username}</p>
+              <p>Email: {email}</p>
+              <p>Phone: {phone}</p>
+              <p>Website: {website}</p>
+            </div>
+          )}
+          <button
+            style={{ margin: "10px" }}
+            onClick={() => this.setState({ inEditMode: !inEditMode })}
+          >
+            {inEditMode && "Save Details" }
+          </button>
+        </div>
       </div>
     );
   }
 }
-    
-    
 
-export default EditProfile
+export default EditProfile;
