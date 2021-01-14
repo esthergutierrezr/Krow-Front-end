6ee56925ced21dd9c1505f799d326bc797bf8c95
LuisClara92 <lc.sbg.92@gmail.com>
2021-01-14 16:27:58 +0000

    correct auth form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import BottomNav from './components/Navigation/BottomNav';
-import Homepage from './components/Home/Homepage';
-import Locations from './components/Locations/Locations';
-import Login from './components/Login/Login';
-import Membership from './components/Membership/Membership';
-import Profile from './components/Profile/Profile';
-import UserContextProvider from './contexts/UserContext';
-import AuthContextProvider, { AuthContext } from './contexts/AuthContext'
-import Signup from './components/Signup/Signup'
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import BottomNav from "./components/Navigation/BottomNav";
+import Homepage from "./components/Home/Homepage";
+import Locations from "./components/Locations/Locations";
+import Login from "./components/Login/Login";
+import Membership from "./components/Membership/Membership";
+import Profile from "./components/Profile/Profile";
+import UserContextProvider from "./contexts/UserContext";
+import AuthContextProvider, { AuthContext } from "./contexts/AuthContext";
+import Signup from "./components/Signup/Signup";
+import "./App.css";
 
 const ProtectedRoute = ({ component: Component, ...rest }) => {
   const { auth } = React.useContext(AuthContext);
@@ -21,25 +21,25 @@ const ProtectedRoute = ({ component: Component, ...rest }) => {
       }
     />
   );
- };
+};
 
 function App() {
   return (
     <div className="App">
       <AuthContextProvider>
-      <UserContextProvider>
-        <Switch>
-          <Route exact path="/" component={Homepage} />
-          <Route path="/locations" component={Locations} />
-          <Route path="/login" component={Login} />
-          <Route path="/membership" component={Membership} />
+        <UserContextProvider>
+          <Switch>
+            <Route exact path="/" component={Homepage} />
+            <Route path="/locations" component={Locations} />
+            <Route path="/login" component={Login} />
+            <Route path="/membership" component={Membership} />
             <Route path="/login" component={Login} />
             <Route path="/signup" component={Signup} />
             <ProtectedRoute path="/profile" component={Profile} />
             {/* <Redirect to="/" /> */}
           </Switch>
-        <BottomNav />
-      </UserContextProvider>
+          <BottomNav />
+        </UserContextProvider>
       </AuthContextProvider>
     </div>
   );
