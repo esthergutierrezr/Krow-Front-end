eaf6fea019d5e5f9da404902ca02ea5a9dbf3f8a
esthergutierrezr <your-email-address>
2021-01-20 11:16:57 +0000

    context locations added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import "./App.css";
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 import BottomNav from "./components/Navigation/BottomNav";
 import Homepage from "./components/Home/Homepage";
-import MainLocations from "./components/Locations/MainLocations";
+import Locations from "./components/Locations/Locations";
 import Login from "./components/Login/Login";
 import Membership from "./components/Membership/Membership";
 import Profile from "./components/Profile/Profile";
-
-import { Switch, Route } from "react-router-dom";
+import UserContext from "./contexts/UserContext";
+import LocationProvider from "./contexts/LocationContext";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route path="/locations" component={MainLocations} />
-
-        <Route path="/login" component={Login} />
-        <Route path="/membership" component={Membership} />
-        <Route path="/profile" component={Profile} />
-      </Switch>
-      <BottomNav />
+      <UserContext>
+        <LocationProvider>
+          <Switch>
+            <Route exact path="/" component={Homepage} />
+            <Route path="/locations" component={Locations} />
+            <Route path="/login" component={Login} />
+            <Route path="/membership" component={Membership} />
+            <Route path="/profile" component={Profile} />
+          </Switch>
+          <BottomNav />
+        </LocationProvider>
+      </UserContext>
     </div>
   );
 }
