bcf06a9e72559d4112a4945c57c703617688089e
Leonvdk <leonvandeklundert@gmail.com>
2021-01-26 23:38:59 +0000

    user gets saved and on second click, sent to home. This bug has to be killed.

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,33 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
 import PhoneInput from "react-phone-input-2";
+import Cookies from "js-cookie";
+import { AuthContext } from '../../contexts/AuthContext';
 import "react-phone-input-2/lib/style.css";
 import "./signup.css";
 
 function Signup() {
   const { register, errors, handleSubmit } = useForm();
+  const { setUser, setAuth } = useContext(AuthContext)
   const history = useHistory();
   const [state, setState] = useState({
     phone: "",
   })
+
+  const [loginInfo, setLoginInfo] = useState({})
 
   const onSubmit = (data) => {
     console.log("data: ",data);
     axios
       .post("/auth/signup", data)
       .then(response => {
-        alert(response.data.flash); 
-        console.log(response); 
+        console.log("on reg.: ", response.data);
+        setLoginInfo({email: data.email, password: data.password})
       })
+      .then(
+        axios.post("/auth/login", loginInfo)
+        .then((response) => {
+          console.log(loginInfo);
+          console.log("on login: ", response);
+          setUser(response.data.foundUser);
+          setAuth(true);
+          Cookies.set("authToken", response.data.token);
+          history.push("/");
+        })
+      )
       .catch(error => console.error(error))
     };
-
-  const handlePhoneNumber=(event)=>{
-    // setPhoneNumber(event.target.value);
-    console.log(phoneNumber)
-  }
 
 
   return (
@@ -45,18 +56,18 @@ function Signup() {
           <PhoneInput
             value={state.phone}
             onChange={phone => setState({ phone })}
-            type = "tel"
+            type="tel"
             country={"pt"}
-            name ="phone"
+            name="phone"
             placeholder={
               !errors.phone ? "phone number" : "Your phone number is required"
             }
           />
           <input
-            className = "hidden"
+            className="hidden"
             value={state.phone}
             type="tel"
-            name="phone"
+            name="phoneNumber"
             placeholder={
               !errors.phone ? "phone number" : "Your phone number is required"
             }
