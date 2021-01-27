0d58ede8d00e1335ace088cb1aaeb25e96bd6161
esthergutierrezr <your-email-address>
2021-01-27 13:49:38 +0000

    location details frop downs added

--- src/components/Locations/LocationDetails/LocationDetails.js
+++ src/components/Locations/LocationDetails/LocationDetails.js
@@ -6,6 +6,27 @@ import { LocationContext } from "../../../contexts/LocationContext.js";
 
 function LocationDetails() {
   const { location } = React.useContext(LocationContext);
+  const [moreTextA, setMoreTextA] = React.useState(false);
+  const [moreTextB, setMoreTextB] = React.useState(false);
+  const [moreTextC, setMoreTextC] = React.useState(false);
+  const [moreTextD, setMoreTextD] = React.useState(false);
+  const [moreTextE, setMoreTextE] = React.useState(false);
+
+  const readMoreA = () => {
+    setMoreTextA(!moreTextA);
+  };
+  const readMoreB = () => {
+    setMoreTextB(!moreTextB);
+  };
+  const readMoreC = () => {
+    setMoreTextC(!moreTextC);
+  };
+  const readMoreD = () => {
+    setMoreTextD(!moreTextD);
+  };
+  const readMoreE = () => {
+    setMoreTextE(!moreTextE);
+  };
 
   return (
     <div>
@@ -24,33 +45,57 @@ function LocationDetails() {
         <a href={`/locations/`}>Back</a>
         <div>
           <h1>{location.selectedPlace.name}</h1>
+          <p>1 miembro aqui</p>
           <p>icon + opening times</p>
           <p>Icon + Location</p>
-          <h2>Overview</h2>
-          <p>{location.selectedPlace.overview}</p>
+          <div> * * * * 4.9</div>
 
           <div>
-            <h2>Amenities</h2>
-            <h4>Beverages Incruded</h4>
-          </div>
-          <div>
-            <h2>Important Info</h2>
-            <h4>Food Policy</h4>
-            <p></p>
-            <h4>Check-In Instructions</h4>
-            <p></p>
-          </div>
-          <div>
-            <h1>Letswort Timings</h1>
-          </div>
-          <div>
-            <h1>Venue Location</h1>
-            <link></link>
-            <link></link>
+            <h2>Description</h2>
+            <p>{location.selectedPlace.overview}</p>
+            <button onClick={readMoreA}>Ler mais</button>
+            {moreTextA ? <p>Here is more info about the place</p> : null}
             <div>
-              <LocationMap />
+              <h2>Facilidades</h2>
+              <button onClick={readMoreB}>more</button>
+              {moreTextB ? <p>Here is the important info </p> : null}
             </div>
-            <button onClick={location.checkIn}>Check In</button>
+            <div>
+              <h2>Informacoes Importantes</h2>
+              <button onClick={readMoreC}>more</button>
+              {moreTextC ? <p>Here is the important info </p> : null}
+            </div>
+            <div>
+              <h2>Beneficios Exclusivos</h2>
+              <button onClick={readMoreD}>more</button>
+              {moreTextD ? <p>Here is the important info </p> : null}
+            </div>
+            <div>
+              <h2>Horarios</h2>
+              <button onClick={readMoreE}>more</button>
+              {moreTextE ? (
+                <div>
+                  {" "}
+                  <p>Segunda </p>
+                  <p>Terca </p>
+                  <p>Quarta </p>
+                  <p>Quinta </p>
+                  <p>Sexta </p>
+                  <p>Sabado </p>
+                  <p>Domingo </p>{" "}
+                </div>
+              ) : null}
+            </div>
+
+            <div>
+              <h1>Sobre esta localizacao</h1>
+              <button>+4876557755</button>
+              <a href="https://google.com">Visitar Website</a>
+              <div>
+                <LocationMap />
+              </div>
+              <button onClick={location.checkIn}>Check In</button>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Locations/Locations.js
+++ src/components/Locations/Locations.js
@@ -17,12 +17,38 @@ function Locations() {
     setShowMap(!map);
   };
 
+  // const showCurrentLocation = () => {
+  //   // Try HTML5 geolocation.
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition(
+  //       (position: Position) => {
+  //         const pos = {
+  //           lat: position.coords.latitude,
+  //           lng: position.coords.longitude,
+  //         };
+
+  //         infoWindow.setPosition(pos);
+  //         infoWindow.setContent("Location found.");
+  //         infoWindow.open(map);
+  //         map.setCenter(pos);
+  //       },
+  //       () => {
+  //         handleLocationError(true, infoWindow, map.getCenter());
+  //       }
+  //     );
+  //   } else {
+  //     // Browser doesn't support Geolocation
+  //     handleLocationError(false, infoWindow, map.getCenter());
+  //   }
+  // }
+
   return (
     <div>
       <h1>Locations</h1>
       <SearchInput />
       <button onClick={changeView}>List</button>
       <button onClick={changeView}>Map</button>
+      {/* <button onClick={showCurrentLocation}>Current location</button> */}
       {map ? <MapContainer /> : <ListContainer />}
     </div>
   );
