84d84a22f0c548b55032849ada6489dda7a9924c
LuisClara92 <lc.sbg.92@gmail.com>
2021-01-29 13:23:01 +0000

    style most part of homepage

--- src/components/Home/Homepage.js
+++ src/components/Home/Homepage.js
@@ -1,31 +1,37 @@
-import React, { useContext } from 'react'
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import Greeting from "./greeting/Greeting";
 import CTAButtons from "./CTAButtons";
 import CommunityUpdates from "./communityUpdates/CommunityUpdates";
 import FeaturedLocations from "./featuredLocations/FeaturedLocations";
 import ImportantMessage from "./ImportantMessage";
-import { AuthContext } from '../../contexts/AuthContext'
-import './homepage.css'
+import { AuthContext } from "../../contexts/AuthContext";
+import "./homepage.css";
+import { HeadersBg } from "../Style/Backgrounds.jsx";
 
 const Homepage = () => {
-  const { user } = useContext(AuthContext)
-    return (
-      <div homepage>
-        <Greeting />
-        <h2>Welcome to Krow.Works</h2>
+  const { user } = useContext(AuthContext);
+  return (
+    <div homepage>
+      <HeadersBg>
+      <h2 className="welcome">Welcome to Krow</h2>
+      <Greeting />
+      </HeadersBg>
+      <div className="bg-white">
         <CTAButtons />
         <h2>Community updates</h2>
         <CommunityUpdates />
         <p className="home__featured-locations">
           <h2>Featured Locations</h2>
-          <Link id="home__view-all-locations" to="/locations">view all</Link>
+          <Link id="home__view-all-locations" to="/locations">
+            view all
+          </Link>
         </p>
         <FeaturedLocations />
         <ImportantMessage />
-
       </div>
-    )
-}
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
