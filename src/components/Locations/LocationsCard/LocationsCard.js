84730a05662f1afe43ed4544254b7f4518efe2ec
esthergutierrezr <your-email-address>
2021-01-29 12:53:00 +0000

    locations cards appearing in map and map moving

--- src/components/Locations/LocationsCard/LocationsCard.js
+++ src/components/Locations/LocationsCard/LocationsCard.js
@@ -1,25 +1,64 @@
 import React from "react";
+import "./LocationsCard.css";
+import { LocationContext } from "../../../contexts/LocationContext";
 
 function LocationsCard(props) {
-  console.log(props);
-  return (
-    <div>
-      <h1>{props.selectedPlace.name}</h1>
-      <img
-        src={props.selectedPlace.image}
-        alt="image"
-        width="250"
-        height="220"
-      />
-      <a href={`/locations/${props.selectedPlace.id}`}>Details</a>
+  const { checked, setChecked } = React.useContext(LocationContext);
+  const { location, setLocation } = React.useContext(LocationContext);
 
-      <div>
-        <p>Wifi Details</p>
-        <p>Network:{props.selectedPlace.network}</p>
-        <p>Password:{props.selectedPlace.password}</p>
-      </div>
-      <div>Closing at </div>
-      <button>Check In</button>
+  const checkIn = () => {
+    setChecked(props.id);
+  };
+  const checkOut = () => {
+    setChecked(null);
+  };
+
+  const onCardClick = () => {
+    console.log(props);
+    setLocation({
+      ...location,
+      center: {
+        lat: props.lat,
+        lng: props.lng,
+      },
+    });
+  };
+
+  return (
+    <div className="locationsCard">
+      {checked !== props.id ? (
+        <div className="checkin">
+          <div>
+            <h1>{props.name}</h1>
+            <p>1 miembro aqui</p>
+
+            <div>09:00 - 17:00 </div>
+            <div> Sintra | Obtener direccoes </div>
+            <div> * * * * 4.9</div>
+            <a href={`/locations/${props.id}`}>Details</a>
+          </div>
+          <button onClick={onCardClick}>
+            <img src={props.image} alt="image" width="150" height="150" />
+          </button>
+          <button onClick={checkIn}>Check In</button>
+        </div>
+      ) : (
+        <div className="checkout">
+          <div>
+            <h1>{props.name}</h1>
+            <p>6 miembro(s) aqui</p>
+            <div>Aberto ate as 22:00 </div>
+            <p>wifi:{props.network}</p>
+            <p>Password:{props.password}</p>
+            <a href={`/locations/${props.id}`}>Details</a>
+          </div>
+
+          <div>
+            <img src={props.image} alt="image" width="250" height="220" />
+          </div>
+          <button onClick={checkOut}>Check Out</button>
+        </div>
+      )}
     </div>
   );
 }
