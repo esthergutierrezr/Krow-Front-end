583b1057d47980a20c0ca1ba35b239921d625227
Leonvdk <leonvandeklundert@gmail.com>
2021-02-04 23:06:02 +0100

    all stars now have a working onClick

--- src/components/Reusable/StarRating.js
+++ src/components/Reusable/StarRating.js
@@ -25,10 +25,10 @@ function StarRating(props) {
     <div className="rating">
 
       <span onMouseEnter={()=>handleHover(1)} onClick={()=>handleClick(1)} onMouseLeave={()=>setRatingOnHover(null)} className={`fa star fa-star${ratingOnHover != null?'':'-o'}`} />
-      <span onMouseEnter={()=>handleHover(2)} onMouseLeave={()=>setRatingOnHover(null)} className={`fa star fa-star${ratingOnHover > 1?'':'-o'}`} />
-      <span onMouseEnter={()=>handleHover(3)} onMouseLeave={()=>setRatingOnHover(null)} className={`fa star fa-star${ratingOnHover > 2?'':'-o'}`} />
-      <span onMouseEnter={()=>handleHover(4)} onMouseLeave={()=>setRatingOnHover(null)} className={`fa star fa-star${ratingOnHover > 3?'':'-o'}`} />
-      <span onMouseEnter={()=>handleHover(5)} onMouseLeave={()=>setRatingOnHover(null)} className={`fa star fa-star${ratingOnHover > 4?'':'-o'}`} />
+      <span onMouseEnter={()=>handleHover(2)} onClick={()=>handleClick(2)} onMouseLeave={()=>setRatingOnHover(null)} className={`fa star fa-star${ratingOnHover > 1?'':'-o'}`} />
+      <span onMouseEnter={()=>handleHover(3)} onClick={()=>handleClick(3)} onMouseLeave={()=>setRatingOnHover(null)} className={`fa star fa-star${ratingOnHover > 2?'':'-o'}`} />
+      <span onMouseEnter={()=>handleHover(4)} onClick={()=>handleClick(4)} onMouseLeave={()=>setRatingOnHover(null)} className={`fa star fa-star${ratingOnHover > 3?'':'-o'}`} />
+      <span onMouseEnter={()=>handleHover(5)} onClick={()=>handleClick(5)} onMouseLeave={()=>setRatingOnHover(null)} className={`fa star fa-star${ratingOnHover > 4?'':'-o'}`} />
 
       <span className="">{` ${ratingOnHover != null? ratingOnHover: "" }`}</span>
     </div>
