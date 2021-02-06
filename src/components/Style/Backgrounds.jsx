eb0791cf7ad1fde1f99ef05346e43e267280fbf0
LuisClara92 <lc.sbg.92@gmail.com>
2021-02-06 15:16:50 +0000

    add styel folder to location branch

---
+++ src/components/Style/Backgrounds.jsx
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+import WhiteWall from "./bg/white_wall.png";
+import Laptop from "./bg/computer-bg.png";
+import { Black, HeaderGrey, Red, Blue } from "./Colors";
+
+export const HeadersBg = styled.div`
+  // width: 100%;
+  height: 112.2px;
+  margin: 0 0 43.8px;
+  padding: 22px 41.3px 21.5px 41px;
+  background-color: ${HeaderGrey};
+`;
+
+export const HeadersProfile = styled.div`
+  width: 100%;
+  height: 112.2px;
+  margin: 0 0 43.8px 0;
+  padding: 22px 41.3px 21.5px 41px;
+  background-color: ${Blue};
+`;
+export const FooterBg = styled.div`
+  width: 479.8px;
+  height: 82.6px;
+  margin: 72.8px 0.2px 0.4px 0;
+  padding: 19.2px 221.1px 11.2px 223.2px;
+  background-color: ${Black};
+`;
+export const WhiteWallBg = styled.div`
+  position: relative;
+  width: 480px;
+  height: 130.3px;
+  align-self: center;
+  background-size: contain;
+  background: url(${WhiteWall});
+`;
+export const LaptopBg = styled.img`
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  align-self: center;
+  background-size: contain;
+  background: url(${Laptop});
+`;
+export const InviteBg = styled.div`
+  width: 480px;
+  height: 722.7px;
+  margin: 63.1px 0 0;
+  padding: 193.6px 0 0 0.7px;
+  background-color: ${Red};
+`;
---
+++ src/components/Style/Colors.jsx
@@ -0,0 +1,45 @@
+// Colors patterns
+
+// NAV | Text Button
+export const Black = '#000000';
+
+export const White = '#FFFFFF';
+
+export const Yellow = '#F8C861';
+
+// Button | Nav Selected Icon
+export const YellowButton = '#F8C861';
+
+export const BlackButton = '#181812';
+
+// Nav unselected Icon
+export const Icon = '#787878';
+
+// Profile
+// Inputs
+export const GrayBorder = '#323232';
+
+// Normal Gray Text
+export const GrayText = '#878787';
+
+// Buy Membership
+export const BlueMembership = '#0d726c';
+
+// BorderBottom Menu Profile
+export const WhiteBorder = '#606060'
+
+// Text Inputs
+export const TextInput = '#898884';
+
+// Headers
+export const HeaderGrey = "#F1F0F0";
+
+// HomePage
+// Subscribe and Profile Header
+export const Blue = '#08726C';
+
+// Invite Friend
+export const Red = '#A73F25';
+
+// Button Shadow
+export const Shadow = 'box-shadow: 0 9px 5px 0 rgba(0, 0, 0, 0.2);'
