a47abd57a86eea8432cd585e293f6dc150c9da0a
LuisClara92 <lc.sbg.92@gmail.com>
2021-02-09 14:00:12 +0000

    try to inset errors of change form using useForm

--- src/components/Profile/ChangePassword.jsx
+++ src/components/Profile/ChangePassword.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-alert */
-import React, { useState, useContext } from "react";
+import React, { useRef, useState, useContext } from "react";
+import { useForm } from "react-hook-form";
 import axios from "axios";
 import { useTranslation } from "react-i18next";
 import { useHistory, Link } from "react-router-dom";
@@ -11,63 +12,59 @@ import "./profile.css";
 // body of backend current_password, new_password, user_id
 
 const ChangePassword = () => {
-  const { user, setUser } = useContext(AuthContext);
-  // const [new_password, setPassword] = useState({});
+  const { user } = useContext(AuthContext);
   const { t } = useTranslation(["profile"]);
+  // const [newPassword, setPassword] = useState({});
   const [fields, setFieldChange] = useState({
-    password: "",
-    oldPassword: user.password,
+    newPassword: "",
+    oldPassword: "",
     confirmPassword: "",
   });
   const id = Number(user.id);
   const history = useHistory();
+  const { register, errors, handleSubmit, watch } = useForm({});
+  // const password = useRef("");
+  // password.current = watch("newPassword", "");
+
+  // fields that i need to backend
+  // const { currentPassword, newPassword, userId } = req.body;
+
+  // how to compare in front end if old password it's teh same
+
+  //
 
   const handleChange = (event) => {
-    console.log("name", event.target.name);
-    console.log("value", event.target.value);
-    // setFieldChange({ [event.target.name]: event.target.value });
+    // console.log("name", event.target.name);
+    // console.log("value", event.target.value);
+    setFieldChange({ [event.target.name]: event.target.value });
   };
 
-  const changePassword = (event) => {
-      event.preventDefault();
-      setFieldChange({ oldPassword: event.target.value, confirmPassword: event.target.value });
-    }
+  // ******try 3
+  const onSubmit = (event, data) => {
+    console.log("data: ", data);
+    // event.preventDefault();
 
-  const handleSubmit = (event) => {
-    if (fields.password !== fields.confirmPassword){
-      alert("The passwords doesn't match")
-    }else{
-      const currentPassword = fields.oldPassword;
-      const newPassword = fields.password;
-      const userId = user.id;
-      axios
-         .post("/password/change", [currentPassword, newPassword, userId])
-         .catch((error) => console.error(error)); // The form will submit
-    }
-    alert("Password has been Changed Successfully");
-    history.push(`/profile/${id}`);
+    // const currentPassword = fields.oldPassword;
+    // const newPassword = fields.password;
+    // const userId = id;
+
+    // axios
+    //   .put("/password/change", [currentPassword, newPassword, userId])
+    //   .then(async (response) => {
+    //     console.log("response: ", response);
+    //     // const user = await setUser(response.data[0]);
+    //     // history.push(`/profile/${id}`);
+    //   })
+    //   .catch((error) => console.error(error));
+    // alert("Password has been Changed Successfully");
   };
-  // const handleSubmit = () => {
-  //   axios
-  //     .post("/password/change", new_password)
-  //     .then((response) => {
-  //       // console.log("response",response);
-  //     })
-  //     .catch((error) => console.error(error));
-  //   alert("Password has been Changed Successfully");
-  //   history.push(`/profile/${id}`);
-  // };
 
   return (
     <div className="bg-white-profile">
       <Content>
         <UserChange />
         <DivEdit>
-          <FormChange
-            onSubmit={(event) => {
-              changePassword(event);
-            }}
-          >
+          <FormChange onSubmit={handleSubmit(onSubmit)}>
             <h1>Password</h1>
             <input
               onChange={handleChange}
@@ -75,31 +72,48 @@ const ChangePassword = () => {
               name="oldPassword"
               placeholder={t("profile:changePassword.old")}
               value={fields.oldPassword}
+              // ref={register({ required: true })}
+              // ref={register({ required: true, validate: (value) => value === user.password || "This password do not match with your old password" })}
             />
             <br />
             <input
-              onChange={handleChange}
-              type="password"
               name="newPassword"
               placeholder={t("profile:changePassword.new")}
-              value={fields.password}
+              onChange={handleChange}
+              type="password"
+              value={fields.newPassword}
+              ref={register({
+                minLength: 8,
+                required: true,
+                pattern: /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$/i,
+                // message: "Password must have at least 8 characters",
+              })}
             />
             <br />
             <input
-              onChange={handleChange}
-              type="password"
               name="confirmPassword"
               placeholder={t("profile:changePassword.confirm")}
+              onChange={handleChange}
+              type="password"
               value={fields.confirmPassword}
+              ref={register({
+                required: true,
+                validate: (value) => console.log("Confirm value:",value, "objective:",password.current) 
+                   || "The passwords do not match",
+              })}
+              // value === password.current
             />
+            {errors.newPassword && <p>Password must have at least 8 characters</p>}
+            {errors.confirmPassword && <p>The passwords do not match</p>}
             <br />
             <Link to="/password/reset">
-              <h2>{t("profile:changePassword.forget")}?</h2>
+              <h2>
+                {t("profile:changePassword.forget")}
+                ?
+              </h2>
             </Link>
             <br />
-            <Save onClick={() => handleSubmit()} type="submit">
-            {t("profile:changePassword.change")}
-            </Save>
+            <Save type="submit">{t("profile:changePassword.change")}</Save>
           </FormChange>
         </DivEdit>
       </Content>
