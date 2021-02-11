bb68aa97782d0f79958ffc1f627b9174c99736f0
LuisClara92 <lc.sbg.92@gmail.com>
2021-02-11 17:19:42 +0000

    start manage membership

--- src/components/Profile/History.jsx
+++ src/components/Profile/History.jsx
@@ -4,27 +4,25 @@
 // Check-Ins
 // check-in card
 
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
-import Billing from './Billing'
-import CheckIns from './CheckIns'
-
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Billing from "./Billing";
+import CheckIns from "./CheckIns";
 
 // History Billing + Cancel Subscription
-{
-  /*<form method="POST" action="/stripe-webhook/create-customer-portal-session">
-  <button type="submit">Manage billing</button>
-</form>;*/
-}
+
 
 
 const History = () => (
   <div>
+    <form method="POST" action="/stripe-webhook/create-customer-portal-session">
+  <button type="submit">Manage billing</button>
+</form>;
     <Switch>
-      <Route path="/profile/historic/ckeckins" component={CheckIns}/>
+      <Route path="/profile/historic/ckeckins" component={CheckIns} />
       <Route path="/profile/historic/billings" component={Billing} />
     </Switch>
   </div>
-    )
+);
 
-export default History
+export default History;
